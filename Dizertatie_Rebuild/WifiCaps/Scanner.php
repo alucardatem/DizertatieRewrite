34ee99fed0602e071b9cadebaa1209c780461ba7
alucardatem <dan_andrei2005@yahoo.com>
2015-09-07 18:07:02 +0300

    made fix for Extra wifi Data

--- Dizertatie_Rebuild/WifiCaps/Scanner.php
+++ Dizertatie_Rebuild/WifiCaps/Scanner.php
@@ -44,15 +44,48 @@ class Scanner
         foreach ($scanned_directory as $key => $file) {
 
             $data = simplexml_load_file($CaptureFolder . "/" . $file);
+
+            // print_r($data);
             foreach ($data->{'wireless-network'} as $number => $network) {
-                $essid = $network->SSID->essid;
-                $string_Essid_Check = (string)$essid->attributes()->cloaked;
-                if ($string_Essid_Check === 'true') {
+
+                if ($network->SSID->essid === true OR $network->SSID->essid == "" OR $network->SSID->essid === " ") {
                     $essid = "[cloaked]";
+                } else {
+                    $essid = $network->SSID->essid;
                 }
+
+
                 $wifi["AP"]["BSSID"] = (string)$network->BSSID;
                 $wifi["AP"]["ESSID"] = (string)$essid;
-                $wifi["AP"]["Encryption"] = (string)$network->SSID->encryption;
+                $wifi["AP"]["manuf"] = (string)$network->manuf;
+
+                $wifi["AP"]["Encryption"] = array();
+                if (!isset($network->SSID)) {
+                    $wifi["AP"]["Encryption"] = "OPN";
+                } else {
+                    if ((string)$network->SSID->encryption === "None" || (string)$network->SSID->encryption === "" OR (string)$network->SSID->encryption === " ") {
+                        $wifi["AP"]["Encryption"] = "OPN";
+                    } else {
+                        $encryption = "";
+                        foreach ($network->SSID as $Enc) {
+                            foreach ($Enc->encryption as $counterEnc => $EncValue) {
+                                $encryption .= $EncValue . " ";
+                            }
+                            // echo " ";
+
+                        }
+                        $encryption = substr($encryption, 0, -1);
+                        $wifi["AP"]["Encryption"] = $encryption;
+                    }
+
+                }
+
+
+                // print_r($wifi["AP"]);
+
+
+                $wifi["AP"]["Carrier"] = (string)$network->carrier;
+                $wifi["AP"]["Encoding"] = (string)$network->encoding;
                 $wifi["AP"]["TransmissionChannel"] = (string)$network->channel;
                 $wifi["AP"]["Frequency"] = (string)$network->freqmhz;
                 $wifi["AP"]["DateTime"] = date('d-m-Y H:i:s');
@@ -62,6 +95,16 @@ class Scanner
                 foreach ($network->{'wireless-client'} as $client) {
                     $wifi["Client"]["BSSID"] = (string)$network->BSSID;
                     $wifi["Client"]["clientmac"][] = (string)$client->{"client-mac"};
+                    $wifi["Client"]["clientManuf"] = (string)$client->{"client-manuf"};
+                    $wifi["Client"]["channer"] = (string)$client->{"channel"};
+                    $wifi["Client"]["carrier"] = (string)$client->{"carrier"};
+                    $wifi["Client"]["encoding"] = (string)$client->{"encoding"};
+                    foreach ($client->SSID as $Probe) {
+                        $wifi["Client"]["Probe"] = "";
+                        if (isset($Probe->ssid)) {
+                            $wifi["Client"]["Probe"][] = (string)$Probe->ssid;
+                        }
+                    }
                     $wifi["Client"]["lat"] = "";
                     $wifi["Client"]["lng"] = "";
                 }
@@ -74,10 +117,7 @@ class Scanner
 
     }
 
-    function parseCSV($CSV_FILE)
-    {
 
-    }
 
 
 }
@@ -91,6 +131,8 @@ $LoggerError = new Logger($LogErrorFile);
 
 $dataBaseConnection = new MySQLi($LoggerError, $LoggerInfo);
 $data = new Scanner($LoggerError, $LoggerInfo);
+
+
 $ap = new AP($dataBaseConnection->_connection, $LoggerError, $LoggerInfo);
 $client = new Client($ap, $dataBaseConnection->_connection, $LoggerError, $LoggerInfo);
 
@@ -98,6 +140,8 @@ $i = 0;
 
 
 $list = $data->parseXML('captures/');
+
+
 $ap->storeAP($list);
 $client->addClient($list);
 $SEARCH = $ap->searchByEncryption("OPN");
